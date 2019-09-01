0aedb2b468fb11464428c25eb8e5ec1530318070
苏豪 <1766727840@qq.com>
2019-09-01 12:47:38 +0800

    使用位运算交换字符串

--- 反转字符串中的单词3/反转字符串中的单词3/test.c
+++ 反转字符串中的单词3/反转字符串中的单词3/test.c
@@ -1,6 +1,11 @@
-#define _CRT_SECURE_NO_WARNINGS 1
+﻿#define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 #include<string.h>
+//给定一个字符串，你需要反转字符串中每个单词的字符顺序，同时仍保留空格和单词的初始顺序。
+//示例 1 :
+//输入 : "Let's take LeetCode contest"
+//输出 : "s'teL ekat edoCteeL tsetnoc"
+//注意：在字符串中，每个单词由单个空格分隔，并且字符串中不会有任何额外的空格。
 void reverse_string(char *s, int low, int high)
 {
 	char tmp = 0;
