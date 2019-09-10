d2dd157e63481757c97657618092a1ca39250242
苏豪 <1766727840@qq.com>
2019-09-10 11:31:30 +0800

    leetcode

---
+++ 大写转小写/大写转小写/test.c
@@ -0,0 +1,36 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//实现函数 ToLowerCase()，该函数接收一个字符串参数 str，并将该字符串中的大写字母转换成小写字母，之后返回新的字符串。
+//示例 1：
+//输入 : "Hello"
+//输出 : "hello"
+//示例 2：
+//输入 : "here"
+//输出 : "here"
+//示例 3：
+//输入 : "LOVELY"
+//输出 : "lovely"
+char * toLowerCase(char * str)
+{
+	char *ret = str;
+	if (str == NULL)
+	{
+		return NULL;
+	}
+	while (*str)
+	{
+		if (*str >= 65 && *str <= 90)
+		{
+			*str += 32;
+		}
+		str++;
+	}
+	return ret;
+}
+int main()
+{
+	char str[] = "LOVELY";
+	char *ret = toLowerCase(str);
+	printf("%s\n", ret);
+	return 0;
+}
